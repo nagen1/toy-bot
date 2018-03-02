dd7999c68b097a10a6ca79d7e0f3daaf677f2fc0
Nagendra Rao Jogi <rao.jogi@gmail.com>
2018-03-02 22:45:20 +0000

    Changed code to gpiozero libraries as to easy to use, prebuilt classes, functions for device control with interactive gpios mapping. However, the gpios are mapping with real world physical numbers and to library support BCM numbers to make it less complicating when connecting them to board.

---
+++ src/hardware_interact_package/scripts/gpios.py
@@ -0,0 +1,34 @@
+#!usr/bin/env python
+import time
+
+# GPIO numbers mapping based on BCM to physical  board numbers
+# Easy to work with and at the same time using gpiozero lib
+
+gpio3 = 2
+gpio5 = 3
+gpio7 = 4
+gpio8 = 14
+gpio10 = 15
+gpio11 = 17
+gpio12 = 18
+gpio13 = 27
+gpio15 = 22
+gpio16 = 23
+gpio18 = 24
+gpio19 = 10
+gpio21 = 9
+gpio22 = 25
+gpio23 = 11
+gpio24 = 8
+gpio26 = 7
+gpio29 = 5
+gpio31 = 6
+gpio32 = 12
+gpio33 = 13
+gpio35 = 19
+gpio36 = 16 # dc_motor_IN1
+gpio37 = 26 # dc_motor_IN2
+gpio38 = 30 # dc_motor_IN3
+gpio40 = 21 # dc_motor_IN4
+
+
---
+++ src/hardware_interact_package/scripts/interact.py
@@ -0,0 +1,69 @@
+#! /usr/bin/env python
+from gpiozero import LED, PWMOutputDevice
+import time
+from gpios import gpio40, gpio38, gpio37, gpio36
+
+# DC Motors/Wheels GPIO mapping
+dc_motor_IN1 = PWMOutputDevice(gpio36)
+dc_motor_IN2 = PWMOutputDevice(gpio37)
+dc_motor_IN3 = PWMOutputDevice(gpio38)
+dc_motor_IN4 = PWMOutputDevice(gpio40)
+
+
+# Control robot using gpiozero lib
+# Create Robot instance
+
+robot = Robot(left=(gpio36,gpio37), right=(gpio38, gpio40))
+robot.forward()
+robot.backward()
+robot.reverse()
+robot.forward(0.5)
+robot.right()
+robot.left(0.5)
+robot.stop()
+
+# Initialize Wheels
+'''def wheels_init():
+    gpio.setmode(gpio.BOARD)
+    gpio.setup(dc_motor_IN1, gpio.OUT)
+    gpio.setup(dc_motor_IN2, gpio.OUT)
+    gpio.setup(dc_motor_IN3, gpio.OUT)
+    gpio.setup(dc_motor_IN4, gpio.OUT)
+
+def go_forward():
+    wheels_init()
+    gpio.output(dc_motor_IN1, True)
+    gpio.output(dc_motor_IN2, False)
+    gpio.output(dc_motor_IN3, False)
+    gpio.output(dc_motor_IN4, True)
+
+def backward():
+    wheels_init()
+    gpio.output(dc_motor_IN1, False)
+    gpio.output(dc_motor_IN2, True)
+    gpio.output(dc_motor_IN3, True)
+    gpio.output(dc_motor_IN4, False)
+
+def right():
+    wheels_init()
+    gpio.output(dc_motor_IN1, True)
+    gpio.output(dc_motor_IN2, False)
+    gpio.output(dc_motor_IN3, False)
+    gpio.output(dc_motor_IN4, False)
+
+def left():
+    wheels_init()
+    gpio.output(dc_motor_IN1, False)
+    gpio.output(dc_motor_IN2, False)
+    gpio.output(dc_motor_IN3, False)
+    gpio.output(dc_motor_IN4, True)
+
+def pivot_right():
+    wheels_init()
+    gpio.output(dc_motor_IN1, True)
+    gpio.output(dc_motor_IN2, False)
+    gpio.output(dc_motor_IN3, True)
+    gpio.output(dc_motor_IN4, False)
+
+def stop():
+    gpio.cleanup()'''
