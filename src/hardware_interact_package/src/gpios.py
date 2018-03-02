db0729b5df40bc7327da89e8ce8f5fc2cc405602
RPi <rao.jogi@gmail.com>
2018-03-02 08:12:20 -0500

    updates to gpio port numbers

--- src/hardware_interact_package/src/gpios.py
+++ src/hardware_interact_package/src/gpios.py
@@ -1,1 +1,32 @@
-gpio3 = 3 
+#!usr/bin/env python
+import time
+# GPIO  numbers based on BOARD libraries
+
+gpio3 = 3
+gpio5 = 5
+gpio7 = 7
+gpio8 = 8
+gpio10 = 10
+gpio11 = 11
+gpio12 = 12
+gpio13 = 13
+gpio15 = 15
+gpio16 = 16
+gpio18 = 18
+gpio19 = 19
+gpio21 = 21
+gpio22 = 22
+gpio23 = 23
+gpio24 = 24
+gpio26 = 26
+gpio29 = 29
+gpio31 = 31
+gpio32 = 32
+gpio33 = 33
+gpio35 = 35
+gpio36 = 36
+gpio37 = 37
+gpio38 = 38
+gpio40 = 40
+
+
--- src/hardware_interact_package/src/interact.py
+++ src/hardware_interact_package/src/interact.py
@@ -1,6 +1,11 @@
-#import RPi.GPIO as gpio
+#! /usr/bin/env python
+import RPi.GPIO as gpio
 import time
+from gpios import gpio5
 
 # Initialize wheels
-#def wheels_init():
-#    gpio.setmode(gpio.BOARD)
+def wheels_init():
+    gpio.setmode(gpio.BOARD)
+
+print gpio5
+print "Welcome"
